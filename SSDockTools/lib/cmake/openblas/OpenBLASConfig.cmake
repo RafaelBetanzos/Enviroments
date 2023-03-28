555048c09982864e927e3b892aa5405211fc7549
Rafael Betanzos <rafael.betanzossj@gmail.com>
2023-03-28 00:46:28 -0300

    Enviroment eliminado

--- SSDockTools/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.3.21")
-SET(OpenBLAS_INCLUDE_DIRS /home/rbetanzos/git-holcan/enviroments/SSDockTools/include)
-SET(OpenBLAS_LIBRARIES /home/rbetanzos/git-holcan/enviroments/SSDockTools/lib/libopenblas.so)
